9f4fb8ab949c0c5a8851b94076e986945b05184d
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-02-20 21:42:36 +0900

    UI 수정

--- src/components/ProjectSummaryCard.tsx
+++ src/components/ProjectSummaryCard.tsx
@@ -69,7 +69,7 @@ const ProjectSummaryCard = ({ project, overlayId, setOverlayId }: IProjectSummar
             className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] z-50"
             onClick={() => setOverlayId(null)}
           ></div>
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col w-2/3 h-5/6 bg-white rounded-2xl p-7 z-50">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col w-2/3 h-5/6 bg-white rounded-2xl p-10 z-50">
             <MdOutlineKeyboardBackspace
               className="absolute top-7 left-10 z-50 cursor-pointer"
               size={40}
@@ -82,85 +82,83 @@ const ProjectSummaryCard = ({ project, overlayId, setOverlayId }: IProjectSummar
                 <span>{project.startDate}</span> - <span>{project.endDate}</span>
               </p>
             </div>
-            <div className="flex justify-center items-center w-full h-full">
-              <div className="flex flex-col items-center w-1/2">
+            <div className="flex justify-evenly items-center absolute top-7 right-7 gap-5">
+              <a
+                href={project.githubURL}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center items-center rounded-3xl p-2 border-2 border-black hover:bg-[rgba(0,0,0,0.05)]"
+              >
+                <SiGithub size={20} />
+              </a>
+              <a
+                href={project.deployURL}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`flex justify-center items-center rounded-3xl w-28 h-10 text-white ${
+                  project.deployURL ? "bg-blue-500 hover:opacity-90" : "bg-gray-400"
+                }`}
+              >
+                {project.deployURL ? "사이트 가기" : "사이트 준비 중"}
+              </a>
+            </div>
+            <div className="flex flex-col justify-center items-center w-full h-2/5">
+              <div className="flex flex-col items-center h-full rounded-xl overflow-hidden">
                 <img
                   src={`/images/${project.id}/${imgIdx}.png`}
                   alt="프로젝트 이미지"
                   loading="lazy"
+                  className="h-full"
                 />
-                <div className="flex justify-between items-center mt-5 relative">
-                  {imgIdx > 0 && (
-                    <MdArrowBackIos
-                      size={15}
-                      className="absolute -left-5 cursor-pointer"
-                      onClick={viewPrevImg}
-                    />
-                  )}
-                  <p>
-                    <span>{imgIdx + 1}</span> / <span>{project.imgLength + 1}</span>
-                  </p>
-                  {imgIdx < project.imgLength && (
-                    <MdArrowForwardIos
-                      size={15}
-                      className="absolute -right-7 cursor-pointer"
-                      onClick={viewNextImg}
-                    />
-                  )}
-                </div>
+              </div>
+              <div className="flex justify-between items-center mt-5 relative">
+                {imgIdx > 0 && (
+                  <MdArrowBackIos
+                    size={13}
+                    className="absolute -left-5 cursor-pointer"
+                    onClick={viewPrevImg}
+                  />
+                )}
+                <p>
+                  <span>{imgIdx + 1}</span> / <span>{project.imgLength}</span>
+                </p>
+                {imgIdx < project.imgLength - 1 && (
+                  <MdArrowForwardIos
+                    size={13}
+                    className="absolute -right-6 cursor-pointer"
+                    onClick={viewNextImg}
+                  />
+                )}
               </div>
             </div>
-            <hr className="my-4" />
-            <div className="w-full h-full">
+            <hr className="my-5" />
+            <div className="w-full h-3/5">
               <div className="flex gap-10">
-                <div className="w-full relative">
+                <div className="w-full relative overflow-scroll">
                   <p className="flex items-center text-xl font-bold mb-4">
                     <MdLightbulb className="mr-2 text-yellow-500" />
                     개요
                   </p>
-                  <p ref={contentRef}></p>
-                  <div className="flex justify-evenly items-center w-full absolute left-0 bottom-0">
-                    <a
-                      href={project.githubURL}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="flex justify-center items-center rounded-3xl w-32 h-12 border-2 border-black"
-                    >
-                      <p className="flex items-center gap-2">
-                        <SiGithub />
-                        Github
-                      </p>
-                    </a>
-                    <a
-                      href={project.deployURL}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={`flex justify-center items-center rounded-3xl w-32 h-12 text-white ${
-                        project.deployURL ? "bg-blue-500" : "bg-gray-400"
-                      }`}
-                    >
-                      {project.deployURL ? "사이트 가기" : "사이트 준비 중"}
-                    </a>
-                  </div>
+                  <p ref={contentRef} className="break-keep"></p>
                 </div>
-                <div className="flex flex-col gap-10 w-full">
+                <div className="flex flex-col gap-6 w-full">
                   <div>
-                    <p className="flex items-center text-xl font-bold mb-4">
+                    <p className="flex items-center text-xl font-bold mb-3">
                       <MdCheck className="mr-2" size={25} />
                       주요 기능
                     </p>
-                    <div className="flex flex-wrap items-center gap-3">
+                    <div className="flex flex-wrap items-center gap-[0.6rem]">
                       {project.mainFeatures.map((feature) => (
                         <FeatureBox>{feature}</FeatureBox>
                       ))}
                     </div>
                   </div>
                   <div>
-                    <p className="flex items-center text-xl font-bold mb-4">
+                    <p className="flex items-center text-xl font-bold mb-3">
                       <MdCheck className="mr-2" size={25} />
                       기술 스택
                     </p>
-                    <div className="flex flex-wrap items-center gap-3">
+                    <div className="flex flex-wrap items-center gap-[0.6rem]">
                       {project.skills.map((feature) => (
                         <FeatureBox>{feature}</FeatureBox>
                       ))}
