ce90b99b9b734d9870e60d21d3eddbc7fb73d722
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-02-20 21:43:12 +0900

    아카이브 박스에 백그라운드 아이콘 추가

--- src/components/pages/Archives.tsx
+++ src/components/pages/Archives.tsx
@@ -1,3 +1,4 @@
+import { SiGithub, SiTistory } from "react-icons/si";
 import MainSection from "../MainSection";
 import SectionTitle from "../SectionTitle";
 import Wrapper from "../Wrapper";
@@ -6,35 +7,38 @@ const Archives = () => {
   return (
     <MainSection className="bg-purple-300 pt-24">
       <Wrapper>
-        <SectionTitle># My Archives</SectionTitle>
+        <SectionTitle className="mb-36"># My Archives</SectionTitle>
         <div className="flex h-80 gap-5">
-          <div className="w-full h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg">
-            <h4 className="text-3xl font-semibold">Github</h4>
-            <a
-              href="https://github.com/YongJaeHyun"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500"
-            >
-              https://github.com/YongJaeHyun
-            </a>
-            <p className="mt-5">지금까지 해온 프로젝트들을 관리하고 있는 저장소입니다.</p>
-          </div>
-          <div className="w-full h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg">
-            <h4 className="text-3xl font-semibold">Tech Blog</h4>
-            <a
-              href="https://lazyong.tistory.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500"
-            >
-              {" "}
-              https://lazyong.tistory.com/
-            </a>
-            <p className="mt-5">
-              제가 공부하고 배운 것들을 하나하나 정리해 공유하는 지식 저장소입니다.
-            </p>
-          </div>
+          <a
+            href="https://github.com/YongJaeHyun"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full h-full"
+          >
+            <div className="w-full h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg relative">
+              <h4 className="text-3xl font-semibold">Github</h4>
+              <p className="text-blue-500">https://github.com/YongJaeHyun</p>
+              <p className="mt-5">
+                <b>지금까지 해온 프로젝트들을 관리하고 있는 저장소입니다.</b>
+              </p>
+              <SiGithub size={220} className="absolute right-0 bottom-3 opacity-10" />
+            </div>
+          </a>
+          <a
+            href="https://lazyong.tistory.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full h-full"
+          >
+            <div className="w-full h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg relative">
+              <h4 className="text-3xl font-semibold">Tech Blog</h4>
+              <p className="text-blue-500">https://lazyong.tistory.com/</p>
+              <p className="mt-5">
+                <b>제가 공부하고 배운 것들을 하나하나 정리해 공유하는 지식 저장소입니다.</b>
+              </p>
+              <SiTistory size={220} className="absolute right-0 bottom-3 opacity-10" />
+            </div>
+          </a>
         </div>
       </Wrapper>
     </MainSection>
