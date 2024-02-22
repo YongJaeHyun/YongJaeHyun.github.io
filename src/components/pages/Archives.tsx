0fcedc0f784f39020f3ed5094ba4bea47f789e19
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-02-23 08:28:14 +0900

    Archives 페이지 모바일 반응형 추가

--- src/components/pages/Archives.tsx
+++ src/components/pages/Archives.tsx
@@ -3,25 +3,32 @@ import MainSection from "../MainSection";
 import SectionTitle from "../SectionTitle";
 import Wrapper from "../Wrapper";
 
-const Archives = () => {
+interface IArchives {
+  isMobile: boolean;
+}
+
+const Archives = ({ isMobile }: IArchives) => {
   return (
     <MainSection className="bg-purple-300 pt-24">
       <Wrapper>
-        <SectionTitle className="mb-36"># My Archives</SectionTitle>
-        <div className="flex h-80 gap-5">
+        <SectionTitle className="mb-20 lg:mb-36"># My Archives</SectionTitle>
+        <div className="flex flex-col lg:flex-row h-80 gap-10 lg:gap-5">
           <a
             href="https://github.com/YongJaeHyun"
             target="_blank"
             rel="noopener noreferrer"
             className="w-full h-full"
           >
-            <div className="w-full h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg relative">
-              <h4 className="text-3xl font-semibold">Github</h4>
+            <div className="w-full h-52 lg:h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg relative">
+              <h4 className="text-2xl lg:text-3xl font-semibold">Github</h4>
               <p className="text-blue-500">https://github.com/YongJaeHyun</p>
-              <p className="mt-5">
+              <p className="mt-5 break-keep">
                 <b>지금까지 해온 프로젝트들을 관리하고 있는 저장소입니다.</b>
               </p>
-              <SiGithub size={220} className="absolute right-0 bottom-3 opacity-10" />
+              <SiGithub
+                size={isMobile ? 150 : 220}
+                className="absolute right-3 bottom-3 opacity-10"
+              />
             </div>
           </a>
           <a
@@ -30,13 +37,16 @@ const Archives = () => {
             rel="noopener noreferrer"
             className="w-full h-full"
           >
-            <div className="w-full h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg relative">
-              <h4 className="text-3xl font-semibold">Tech Blog</h4>
+            <div className="w-full h-52 lg:h-full bg-white border-t-8 border-black rounded-lg px-6 py-3 shadow-lg relative">
+              <h4 className="text-2xl lg:text-3xl font-semibold">Tech Blog</h4>
               <p className="text-blue-500">https://lazyong.tistory.com/</p>
-              <p className="mt-5">
+              <p className="mt-5 break-keep">
                 <b>제가 공부하고 배운 것들을 하나하나 정리해 공유하는 지식 저장소입니다.</b>
               </p>
-              <SiTistory size={220} className="absolute right-0 bottom-3 opacity-10" />
+              <SiTistory
+                size={isMobile ? 150 : 220}
+                className="absolute right-3 bottom-3 opacity-10"
+              />
             </div>
           </a>
         </div>
