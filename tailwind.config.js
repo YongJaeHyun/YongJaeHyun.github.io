cc23640a9dd8bfc758f36762c08e2adb2cfecd2b
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-02-29 00:45:35 +0900

    typing blink 애니메이션과 손 흔드는 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,34 @@ export default {
       boxShadow: {
         aboutCard: "rgba(0, 0, 0, 0.2) 40px 60px 40px -5px",
       },
+      animation: {
+        blink: "blink 1s steps(2, start) infinite",
+        hello: "hello 2s ease-out infinite",
+      },
+      keyframes: {
+        blink: {
+          to: { visibility: "hidden" },
+        },
+        wave: {
+          "0%": {
+            transform: "translate3d(-90px, 0, 0)",
+          },
+          "100%": {
+            transform: "translate3d(85px, 0, 0)",
+          },
+        },
+        hello: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "80%": {
+            transform: "rotate(25deg)",
+          },
+          "100%": {
+            transform: "rotate(0deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
