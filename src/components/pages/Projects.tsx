c1efe737e18a44b9b175c8b5b3c8dca7fef05481
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-01 01:54:54 +0900

    프로젝트 요약 페이지 추가 요약 페이지에서 각각의 프로젝트로 이동 기능 추가 이동 후 뒤로가기 시 프로젝트 요약 페이지로 이동

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,21 +1,54 @@
-import { useState } from "react";
 import MainSection from "../MainSection";
 import SectionTitle from "../SectionTitle";
 import Wrapper from "../Wrapper";
 import projects from "../../projects";
 import Project from "./Project";
+import ProjectSummaryCard from "../projects/ProjectSummaryCard";
+import { useCallback, useEffect } from "react";
 
 interface IProjects {
   isMobile: boolean;
+  moveToOtherSlide: (amount: number) => void;
 }
 
-const Projects = ({ isMobile }: IProjects) => {
+const Projects = ({ isMobile, moveToOtherSlide }: IProjects) => {
+  const moveToTargetSlide = (targetId: number) => {
+    history.pushState(null, "");
+    moveToOtherSlide(targetId + 4);
+  };
+
+  const moveToProjectSummarySlide = useCallback(() => {
+    moveToOtherSlide(3);
+  }, [moveToOtherSlide]);
+
+  useEffect(() => {
+    window.addEventListener("popstate", moveToProjectSummarySlide);
+    return () => {
+      window.removeEventListener("popstate", moveToProjectSummarySlide);
+    };
+  }, [moveToProjectSummarySlide]);
+
   return (
     <MainSection className="bg-indigo-300 pt-24 !h-auto">
       <Wrapper>
-        <SectionTitle className={`md:sticky md:top-24`}># Projects</SectionTitle>
+        <SectionTitle
+          className={`md:sticky md:top-24 cursor-pointer`}
+          onClick={() => moveToOtherSlide(3)}
+        >
+          # Projects
+        </SectionTitle>
+        <MainSection className="grid w-full grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 gap-5">
+          {projects.map((project) => (
+            <ProjectSummaryCard
+              key={project.id}
+              projectId={project.id}
+              title={project.title}
+              moveToTargetSlide={() => moveToTargetSlide(project.id)}
+            />
+          ))}
+        </MainSection>
         {projects.map((project) => (
-          <Project key={project.id} project={project} />
+          <Project key={project.id + project.title} project={project} />
         ))}
       </Wrapper>
     </MainSection>
---
+++ src/components/projects/ProjectSummaryCard.tsx
@@ -0,0 +1,30 @@
+interface IProjectSummaryCard {
+  projectId: number;
+  title: string;
+  moveToTargetSlide: () => void;
+}
+
+const ProjectSummaryCard = ({ projectId, title, moveToTargetSlide }: IProjectSummaryCard) => {
+  return (
+    <>
+      <div
+        className="flex flex-col w-40 md:w-72 h-32 md:h-52 cursor-pointer mt-20 mx-auto"
+        onClick={moveToTargetSlide}
+      >
+        <div className="h-full rounded-xl overflow-hidden mb-3">
+          <img
+            src={`/images/${projectId}/0.png`}
+            alt="프로젝트 대표 이미지"
+            className="h-full object-cover"
+            loading="lazy"
+          />
+        </div>
+        <h4 className="w-full text-md lg:text-lg font-semibold truncate text-center">
+          {title}
+        </h4>
+      </div>
+    </>
+  );
+};
+
+export default ProjectSummaryCard;
