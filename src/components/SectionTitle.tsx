95b52c34f37b05595930b274670d7f493d8f5837
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-01 01:57:26 +0900

    프로젝트 페이지의 제목으로 프로젝트 요약 페이지로 이동하는 기능 추가

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -3,10 +3,15 @@ import { ReactNode } from "react";
 interface ISectionTitle {
   children: ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
-const SectionTitle = ({ children, className }: ISectionTitle) => {
-  return <h2 className={`text-2xl lg:text-3xl font-semibold ${className}`}>{children}</h2>;
+const SectionTitle = ({ children, className, onClick }: ISectionTitle) => {
+  return (
+    <h2 className={`text-2xl lg:text-3xl font-semibold ${className}`} onClick={onClick}>
+      {children}
+    </h2>
+  );
 };
 
 export default SectionTitle;
