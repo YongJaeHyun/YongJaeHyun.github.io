bbc93cfe2033187b7961726bfa86f9b047d809b9
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-15 05:51:01 +0900

    Design: font 크기 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="absolute bottom-10 w-full text-center">
+    <footer className="absolute bottom-10 w-full text-center text-sm md:text-base">
       Copyright &copy; 2023 YongjaeHyun. All rights reserved.
     </footer>
   );
