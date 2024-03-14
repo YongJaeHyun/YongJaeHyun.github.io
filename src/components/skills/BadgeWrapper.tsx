29f21cf19f583560b751620c34ddc3df9146bbbf
dltjrrbs2020 <dltjrrbs2020@gmail.com>
2024-03-15 05:56:54 +0900

    Design: 뱃지 레이아웃 flex-wrap 에서 grid로 변경 및 호버시, rotate 추가

--- src/components/skills/BadgeWrapper.tsx
+++ src/components/skills/BadgeWrapper.tsx
@@ -5,7 +5,11 @@ interface IBadgeWrapper {
 }
 
 const BadgeWrapper = ({ children }: IBadgeWrapper) => {
-  return <div className="flex flex-wrap gap-12 lg:gap-10 p-8 lg:p-10">{children}</div>;
+  return (
+    <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 laptop:grid-cols-7 gap-6 lg:gap-10 p-6 lg:p-10">
+      {children}
+    </div>
+  );
 };
 
 export default BadgeWrapper;
